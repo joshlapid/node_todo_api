0a5804159cb205c7160de35ace4c9cd8afb5a79c
joshlapid <lapidjoshua@gmail.com>
2019-01-16 17:01:08 -0800

    Add get todos route

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 // MIDDLEWARE
 app.use(bodyParser.json());
 
-// CREATE
+// CREATE todos
 app.post('/todos', (req,res) => {
 	const todo = new Todo({
 		text: req.body.text
@@ -21,6 +21,17 @@ app.post('/todos', (req,res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+// GET todos
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({
+			todos: todos
+		});
+	}, (e) => {
+		res.status(400).send(e);
+	})
 });
 
 app.listen(3000, () => {
