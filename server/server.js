a056c878d201099f69094e77e873269abca6c6ec
joshlapid <lapidjoshua@gmail.com>
2019-01-20 16:15:12 -0800

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -31,6 +32,26 @@ app.get('/todos', (req, res) => {
 		});
 	}, (e) => {
 		res.status(400).send(e);
+	})
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	
+	// Validate ID
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send('error');
+	}
+
+	// findById
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo: todo});
+	}, (e) => {
+		res.status(400).send();
 	})
 });
 
