f5c9d789c16fcf23e29e8642ae320277d3e10904
joshlapid <lapidjoshua@gmail.com>
2019-01-21 22:37:15 -0800

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -62,16 +63,44 @@ app.delete('/todos/:id', (req, res) => {
 	const id = req.params.id;
 
 	// validate -> 404
-	if(!ObjectID.isValid(id)) {
+	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
 
 	// remove todo by id
 	Todo.findByIdAndDelete(id).then((todo) => {
-		if(!todo) {
+		if (!todo) {
 			return res.sendStatus(404).send();
 		}
 		res.status(200).send({todo: todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
+// UPDATE /todos/:id
+app.patch('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	// Uses lodash to create body
+	const body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
+		if (!todo) {
+			return res.status(404).send;
+		}
+
+		res.status(200).send({todo});
 	}).catch((e) => {
 		res.status(400).send();
 	});
